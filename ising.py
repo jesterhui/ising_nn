a1806eb9c2ce6a1c07ce12497738e03e6ddbd477
Frank Doherty <35117457+fdoherty@users.noreply.github.com>
2019-04-11 10:55:33 -0400

    Add files via upload
    Added Wolff update step

--- ising.py
+++ ising.py
@@ -25,20 +25,20 @@ class Ising:
         self.size = size
         self.temp = temp
 
+
     def visualize(self, path=None):
         """Visualize and display lattice.
 
         Args:
             path (str): Path to save image to.
 
-        Returns:
-            type: Description of returned object.
 
         """
         plt.imshow(self.lattice, cmap='Greys')
         if path is not None:
             plt.savefig(path, dpi=400, bbox_inches='tight')
         plt.show()
+
 
     def get_h(self):
         """Evaluate Ising spin hamiltonian for lattice.
@@ -65,6 +65,7 @@ class Ising:
         hamiltonian = float(hamiltonian)
         return hamiltonian
 
+
     def get_m(self):
         """Evaluate lattice magnetization.
 
@@ -75,6 +76,7 @@ class Ising:
         mag = np.sum(self.lattice)
         mag = float(mag)
         return mag
+
 
     def metropolis_update(self):
         """Perform single Metropolis-Hastings update step.
@@ -100,18 +102,76 @@ class Ising:
         if delta_e <= 0 or np.random.random() < np.exp(-delta_e / (self.temp)):
             self.lattice[i, j] = -self.lattice[i, j]
 
-    def run(self, iterations):
-        """Run Metropolis-Hastings Monte Carlo.
 
-        Args:
-            iterations (int): Number of update steps to perform.
+    def wolff_update(self):
+        """Perform Wolff cluster update step.
 
         """
-        for _ in range(iterations):
-            self.metropolis_update()
+
+        ind = np.random.randint(low=0, high=self.size, size=(2, 1))
+
+        i = ind[0, 0]
+        j = ind[1, 0]
+
+        root = (i,j)
+        self.build_cluster(root, self.lattice[root])
 
 
-R = Ising(10, 16)
+    def build_cluster(self, site, spin):
+        """Build cluster of sites for Wolff update and flips them through recursive call.
+
+        Args:
+            site (tuple): growing point for cluster.
+
+            spin (int): starting spin for site.
+
+        """
+        self.lattice[site] = -self.lattice[site]  # flip the spin of this site
+        ss = self.size
+        neighbors = [(0, 0), (0, 0), (0, 0), (0, 0)]  # define the neighbors for this site
+        (i, j) = site
+
+        if i == ss - 1:
+            neighbors[0] = (0, j)
+        else:
+            neighbors[0] = (i + 1, j)
+        if i == 0:
+            neighbors[1] = (ss - 1, j)
+        else:
+            neighbors[1] = (i - 1, j)
+        if j == ss - 1:
+            neighbors[2] = (i, 0)
+        else:
+            neighbors[2] = (i, j + 1)
+        if j == 0:
+            neighbors[3] = (i, ss - 1)
+        else:
+            neighbors[3] = (i, j - 1)
+
+        for next_site in neighbors:  # loop over nearest neighbors
+            if self.lattice[next_site] == spin:  # excludes sites already visited which have already been flipped
+                if np.random.random() < 1 - np.exp(-2.0/self.temp):
+                    self.build_cluster(next_site, spin)  # recursively call build_cluster for new sites
+
+
+    def run(self, iterations):
+        """Run Markov Chain Monte Carlo.
+
+        Args:
+            iterations (int): Number of Monte Carlo steps to perform.
+            One Monte Carlo steps includes N Metropolis updates and one Wolff update,
+            where N is the number of lattice sites.
+
+        """
+        sites = (self.size)**2
+
+        for ii in range(iterations):
+            for _ in range(sites):
+                self.metropolis_update()
+            self.wolff_update()
+
+
+R = Ising(2.7, 16)
 R.visualize()
-R.run(10000)
-R.visualize('images/progress_update_high_temp.png')
+R.run(40)
+R.visualize(images/wolff_crit_temp.png)
