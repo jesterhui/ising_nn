f2a442c1ac5fc02b7a7a0f3e807108815b58ab1c
Jacques Esterhuizen <esterhui@umich.edu>
2019-04-13 14:04:08 -0400

    finish up DBN

--- boltzmann_machines.py
+++ boltzmann_machines.py
@@ -1,15 +1,16 @@
+"""
+Generative models used to generate Ising model lattice configurations for
+CHE696 final project.
+"""
 import matplotlib as mpl
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.datasets import fetch_openml
-from sklearn.utils import check_random_state
 
 mpl.rc('font', **{'family': 'serif'})
 mpl.rc('text', usetex=True)
 
 
 class RestrictedBoltzmannMachine:
-    """Restricted Boltzmann Machine.
+    """Restricted Boltzmann machine.
 
     Attributes:
         weights (obj): NumPy array of dimension (h+1, d+1) where h is the
@@ -51,6 +52,8 @@ class RestrictedBoltzmannMachine:
             hidden layer.
 
         """
+        if visible_values.shape[0] == self.weights.shape[1] - 1:
+            visible_values = np.insert(visible_values, 0, 1, axis=0)
         hidden_activations = self.sigmoid(self.weights @ visible_values)
         hidden_activations[0, :] = 1
         h_i, h_j = hidden_activations.shape
@@ -91,19 +94,30 @@ class RestrictedBoltzmannMachine:
         visible_activations, visible_values = self.backward_pass(hidden_values)
         return visible_activations, visible_values
 
-    def contrastive_divergence(self, x, k=10, epochs=100, batch_size=100):
+    def contrastive_divergence(self, x, k=10, epochs=100, batch_size=100,
+                               dbn_bias=True):
         """Train restricted boltzmann machine using contrastive divergence.
 
         Args:
             x (obj): NumPy array of dimension (n, d) containing data.
+            k (int): Number of Gibbs sampling steps used during training.
+            Emperically 10 is usually good.
             epochs (int): Number of training epochs.
+            batch_size (int): Size of training batch.
+            dbn_bias (bool): specifies whether to include bias for later layers
+            of DBN.
 
         """
         momentum = 0  # initialize momentum as 0
-        x = np.insert(x, 0, 1, axis=1)  # insert ones row to use as bias unit
+        if dbn_bias is True:
+            x = np.insert(x, 0, 1, axis=1)  # insert ones row for bias unit
+            # initialize weights randomly
+            self.weights = np.random.rand(self.hidden_layers+1, x.shape[1])
+        else:
+            # initialize weights randomly
+            self.weights = np.random.rand(self.hidden_layers, x.shape[1])
+
         n_batches = int(x.shape[0] / batch_size)  # calculate number of batches
-        # initialize weights randomly
-        self.weights = np.random.rand(self.hidden_layers+1, x.shape[1])
         # initialize bias units to have weight 0
         self.weights[0, :] = 0
         self.weights[:, 0] = 0
@@ -139,7 +153,7 @@ class RestrictedBoltzmannMachine:
                            / batch_size) + self.eta * momentum)
                 self.weights += update
                 momentum = update
-
+                """
             # monitor training progress
             if (epoch + 1) % 5 == 0:
                 _, visible_values = self.gibbs(x.T)
@@ -163,7 +177,7 @@ class RestrictedBoltzmannMachine:
                 plt.close()
                 error = self.reconstruction_error(x, visible_activations)
                 print(error)
-
+                """
     @staticmethod
     def sigmoid(z):
         """Sigmoid utility.
@@ -196,27 +210,125 @@ class RestrictedBoltzmannMachine:
         return reconstruction_error
 
 
-np.random.seed()
+class DeepBeliefNetwork:
+    """Deep belief network.
 
-X, y = fetch_openml('mnist_784', return_X_y=True)
-random_state = check_random_state(0)
-permutation = random_state.permutation(X.shape[0])
-X = X[permutation]
-y = y[permutation]
-y = y.astype('float')
-y = y.reshape((y.size, 1))
-X = X.reshape((X.shape[0], -1))
-X[X >= 0.5] = 1
-X[X < 0.5] = 0
-X = X[:10000, :]
-X = X.reshape((X.shape[0], -1))
+    Attributes:
+        weights (obj): NumPy array of dimension (h+1, d+1) where h is the
+        number of hidden layers and d is the number of input features. The +1
+        for each is due to the bias term.
+        alpha (float): Learning rate, which specifies the step size for
+        the contrastive divergence training.
+        eta (float): Value specifying the strength of the momentum term for the
+        contrastive_divergence.
+        hidden_layers_spec (list): List specifying the number of nodes in
+        each hidden layer, in the order of visible -> associative memory.
+        layers (list): List of RestrictedBoltzmannMachine objects used for each
+        layer.
 
-R = RestrictedBoltzmannMachine(144)
-R.contrastive_divergence(X)
-fig, ax = plt.subplots(12, 12)
-for i in range(144):
-    ax.ravel()[i].imshow(R.weights[i, 1:].reshape(28, 28), cmap='coolwarm')
-    ax.ravel()[i].set_xticks([])
-    ax.ravel()[i].set_yticks([])
-plt.tight_layout()
-plt.savefig('images/latent_space.png', dpi=400, bbox_inches='tight')
+    """
+
+    def __init__(self, hidden_layers_spec, alpha=1e-1, eta=0.5):
+        """Pass network, training parameters to class.
+
+        Args:
+            hidden_layers_spec (list): List specifying the number of nodes in
+            each hidden layer, in the order of visible -> associative memory.
+            alpha (float): Learning rate, which specifies the step size for
+            the contrastive divergence training.
+            eta (float): Value specifying the strength of the momentum term for
+            the contrastive_divergence.
+
+        """
+        self.hidden_layers_spec = hidden_layers_spec
+        self.alpha = alpha
+        self.eta = eta
+        self.weights = None
+        self.layers = []
+
+    def forward_pass(self, visible_values, n_associative=20):
+        """Pass visible node values forward through network to hidden layers,
+         and then pass back and forth in the associative memory.
+
+        Args:
+            visible_values (obj): NumPy array containing visible node values.
+            n_associative (int): Number of passes to perform in associative
+            memory.
+
+        Returns:
+            obj: NumPy array containing sampled values for each node in the
+            hidden layer.
+
+        """
+        n_l = len(self.hidden_layers_spec)
+        _, hidden_values = self.layers[0].forward_pass(visible_values)
+        for i in range(1, n_l-1):
+            _, hidden_values = self.layers[i].forward_pass(visible_values)
+        for __ in range(n_associative):
+            _, hidden_values = self.layers[n_l-1].gibbs(hidden_values)
+        _, hidden_values = self.layers[n_l-1].forward_pass(hidden_values)
+        return hidden_values
+
+    def backward_pass(self, hidden_values, n_associative=20):
+        """Pass hidden node values backwards through network to visible layers.
+
+        Args:
+            hidden_values (obj): NumPy array containing hidden node values.
+            n_associative (int): Number of passes to perform in associative
+            memory.
+
+        Returns:
+            obj: NumPy array containing sampled values for each node in the
+            visible layer.
+
+        """
+        n_l = len(self.hidden_layers_spec)
+        for __ in range(n_associative):
+            _, hidden_values = self.layers[n_l-1].backward_pass(hidden_values)
+            _, hidden_values = self.layers[n_l-1].forward_pass(hidden_values)
+        _, hidden_values = self.layers[n_l-1].backward_pass(hidden_values)
+        for i in reversed(range(1, n_l-1)):
+            _, hidden_values = self.layers[i].backward_pass(hidden_values)
+        _, visible_values = self.layers[0].backward_pass(hidden_values)
+        return visible_values
+
+    def recognition_generation(self, visible_values, n_associative=20):
+        """Perform one full pass of the data up and down the DBN.
+
+        Args:
+            visible_values (obj): NumPy array containing visible node values.
+
+        Returns:
+            obj: NumPy array containing new visible node activations
+            obj: NumPy array containing new visible node values.
+
+        """
+        hidden_values = self.forward_pass(visible_values, n_associative / 2)
+        visible_values = self.backward_pass(hidden_values, n_associative / 2)
+        return visible_values
+
+    def greedy_training(self, x, k=10, epochs=100, batch_size=100):
+        """Perform greedy layer-wise training of DBN.
+
+        Args:
+            x (obj): NumPy array of dimension (n, d) containing data.
+            k (int): Number of Gibbs sampling steps used during training.
+            Emperically 10 is usually good.
+            epochs (int): Number of training epochs.
+            batch_size (int): Size of training batch.
+        """
+        n_l = len(self.hidden_layers_spec)
+        r = RestrictedBoltzmannMachine(self.hidden_layers_spec[0],
+                                       self.alpha, self.eta)
+        r.contrastive_divergence(x, k, epochs, batch_size)
+        _, x = r.forward_pass(x.T)
+        x = x.T
+        self.layers.append(r)
+        for i in range(1, n_l):
+            r = RestrictedBoltzmannMachine(self.hidden_layers_spec[i],
+                                           self.alpha, self.eta)
+            r.contrastive_divergence(x, k, epochs, batch_size, dbn_bias=False)
+            _, x = r.forward_pass(x.T)
+            x = x.T
+
+            self.layers.append(r)
